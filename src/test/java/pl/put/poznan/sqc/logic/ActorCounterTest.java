4cca839c68e738ffebf5a666fbba0e3da3eefbd8
sandaczsands <aleksandra.ostrowski.pl@gmail.com>
2025-01-25 00:32:30 +0100

    Added testing classes for ActorCounter Class and ActorSwapper Class

---
+++ src/test/java/pl/put/poznan/sqc/logic/ActorCounterTest.java
@@ -0,0 +1,92 @@
+package pl.put.poznan.sqc.logic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class tests ActorCounter's class functionality using JUnit library.
+ *
+ * @author Aleksandra Ostrowska
+ * @since 7.0
+ */
+class ActorCounterTest {
+    private ActorCounter actorCounter;
+    private SQC.ScenarioBody scenarioBody;
+
+    @BeforeEach
+    void setUp() {
+        actorCounter = new ActorCounter();
+        scenarioBody = new SQC.ScenarioBody();
+
+        scenarioBody.actors = new String[]{"Bibliotekarz", "Uzytkownik"};
+        scenarioBody.system = new String[]{"System"};
+        scenarioBody.scenarios = new SQC.ScenarioDescription[]{
+                new SQC.ScenarioDescription(0, "Bibliotekarz wybiera opcje dodania nowej pozycji książkowej"),
+                new SQC.ScenarioDescription(0, "Wyświetla się formularz."),
+                new SQC.ScenarioDescription(0, "Bibliotekarz podaje dane książki."),
+                new SQC.ScenarioDescription(1, "IF: Bibliotekarz pragnie dodać egzemplarze książki"),
+                new SQC.ScenarioDescription(1, "Bibliotekarz wybiera opcję definiowania egzemplarzy"),
+                new SQC.ScenarioDescription(1, "System prezentuje zdefiniowane egzemplarze"),
+                new SQC.ScenarioDescription(1, "FOR EACH egzemplarz:"),
+                new SQC.ScenarioDescription(2, "Bibliotekarz wybiera opcję dodania egzemplarza"),
+                new SQC.ScenarioDescription(2, "System prosi o podanie danych egzemplarza"),
+                new SQC.ScenarioDescription(2, "Bibliotekarz podaje dane egzemplarza i zatwierdza."),
+                new SQC.ScenarioDescription(2, "System informuje o poprawnym dodaniu egzemplarza i prezentuje zaktualizowaną listę egzemplarzy."),
+                new SQC.ScenarioDescription(0, "Bibliotekarz zatwierdza dodanie książki."),
+                new SQC.ScenarioDescription(0, "System informuje o poprawnym dodaniu książki."),
+                new SQC.ScenarioDescription(0, "")
+        };
+    }
+
+    @Test
+    void test_visit_getInfo_actor_null(){
+        actorCounter.visit(scenarioBody);
+        actorCounter.setActorNames("");
+
+        Integer info = actorCounter.getInfo();
+
+        assertNull(info);
+    }
+
+    @Test
+    void test_visit_getInfo(){
+        actorCounter.visit(scenarioBody);
+        actorCounter.setActorNames("Bibliotekarz");
+
+        Integer info = actorCounter.getInfo();
+
+        assertEquals(7, info);
+    }
+
+    @Test
+    void test_visit_getInfo_actor_zero(){
+        actorCounter.visit(scenarioBody);
+        actorCounter.setActorNames("Sklepikarz");
+
+        Integer info = actorCounter.getInfo();
+
+        assertEquals(0, info);
+    }
+
+    @Test
+    void test_visit_count_Bibliotekarz(){
+        actorCounter.visit(scenarioBody);
+
+        int result = actorCounter.countActor("Bibliotekarz");
+
+        assertEquals(7, result);
+    }
+
+    @Test
+    void test_visit_count_zero(){
+        actorCounter.visit(scenarioBody);
+
+        int result = actorCounter.countActor("Sklepikarz");
+
+        assertEquals(0, result);
+    }
+
+
+}
---
+++ src/test/java/pl/put/poznan/sqc/logic/ActorSwapperTest.java
@@ -0,0 +1,88 @@
+package pl.put.poznan.sqc.logic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class tests ActorSwapper's class functionality using JUnit library.
+ *
+ * @author Aleksandra Ostrowska
+ * @since 7.0
+ */
+class ActorSwapperTest {
+    private ActorSwapper actorSwapper;
+    private SQC.ScenarioBody scenarioBody;
+
+    @Mock
+    private ActorCounter actorCounter;
+
+    @BeforeEach
+    void setUp() {
+        actorSwapper = new ActorSwapper();
+        scenarioBody = new SQC.ScenarioBody();
+
+        actorCounter = mock(ActorCounter.class);
+
+        scenarioBody.actors = new String[]{"Bibliotekarz", "Uzytkownik"};
+        scenarioBody.system = new String[]{"System"};
+        scenarioBody.scenarios = new SQC.ScenarioDescription[]{
+                new SQC.ScenarioDescription(0, "Bibliotekarz wybiera opcje dodania nowej pozycji książkowej"),
+                new SQC.ScenarioDescription(0, "Wyświetla się formularz."),
+                new SQC.ScenarioDescription(0, "Bibliotekarz podaje dane książki."),
+                new SQC.ScenarioDescription(1, "IF: Bibliotekarz pragnie dodać egzemplarze książki"),
+                new SQC.ScenarioDescription(1, "Bibliotekarz wybiera opcję definiowania egzemplarzy"),
+                new SQC.ScenarioDescription(1, "System prezentuje zdefiniowane egzemplarze"),
+                new SQC.ScenarioDescription(1, "FOR EACH egzemplarz:"),
+                new SQC.ScenarioDescription(2, "Bibliotekarz wybiera opcję dodania egzemplarza"),
+                new SQC.ScenarioDescription(2, "System prosi o podanie danych egzemplarza"),
+                new SQC.ScenarioDescription(2, "Bibliotekarz podaje dane egzemplarza i zatwierdza."),
+                new SQC.ScenarioDescription(2, "System informuje o poprawnym dodaniu egzemplarza i prezentuje zaktualizowaną listę egzemplarzy."),
+                new SQC.ScenarioDescription(0, "Bibliotekarz zatwierdza dodanie książki."),
+                new SQC.ScenarioDescription(0, "System informuje o poprawnym dodaniu książki."),
+                new SQC.ScenarioDescription(0, "")
+        };
+    }
+
+    @Test
+    void test_visit_getInfo_actor1_null(){
+        actorSwapper.visit(scenarioBody);
+        actorSwapper.setActorNames("", "Sklepikarz");
+
+        String info = actorSwapper.getInfo();
+        assertNull(info);
+    }
+
+    @Test
+    void test_visit_getInfo_actor2_null(){
+        actorSwapper.visit(scenarioBody);
+        actorSwapper.setActorNames( "Bibliotekarz", "");
+
+        String info = actorSwapper.getInfo();
+        assertNull(info);
+    }
+
+    @Test
+    void test_visit_getInfo_actors_equal(){
+        actorSwapper.visit(scenarioBody);
+        actorSwapper.setActorNames( "Bibliotekarz", "Bibliotekarz");
+
+        String info = actorSwapper.getInfo();
+        assertNull(info);
+    }
+
+    @Test
+    void test_visit_getInfo_invalid_actor_null(){
+        actorSwapper.visit(scenarioBody);
+        actorSwapper.setActorNames( "Sklepikarz", "Sklepikarz1");
+
+        String info = actorSwapper.getInfo();
+        assertNull(info);
+    }
+    
+
+
+}
